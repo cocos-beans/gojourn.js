3c4917f6c93803665521ea6be42c93724242874f
Blair Myhre <120442257+cocos-beans@users.noreply.github.com>
2023-12-11 10:22:17 -0600

    Delete gojourn directory

--- gojourn/GIL/index.js
+++
@@ -1,7 +0,0 @@
-const Character = require('./objects/character.js')
-const Item = require('./objects/item.js')
-const Location = require('./objects/location.js')
-const Player = require('./objects/player.js')
-const Action = require('./action.js')
-
-const Act = new Action()
