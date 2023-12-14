fc5bdb5c359f708add2d83faf0610912962646ce
Blair Myhre <120442257+cocos-beans@users.noreply.github.com>
2023-12-14 14:34:15 -0600

    Main menu set up

--- gojourn/build.js
+++ gojourn/build.js
@@ -1,24 +1,32 @@
 const objects = require('./object.js')
 
-let main_menu = new objects.Location("main_menu", {name: "mainmenu", desc: ""}, [], {north: "cell01", south: "credits"})
-let player = new objects.Actor("player", {name: "player", desc: ""}, [], "main_menu", [], [], 0, 0, 0, false, 0)
+let main_menu = new objects.Location("main_menu", {name: "mainmenu"}, [], {north: "cell01", south: "credits"})
+let credits = new objects.Location("credits", {name: "credits"}, [], {north: "main_menu"})
+let cell01 = new objects.Location("cell01", {name: "Prisoner Cell", desc: "This is the cell to which you are bound. It's gross."}, ["cell", "prisonercell"], {north: "hallway01"})
 
-let select = new objects.Action("select", "<option>", "Selects an option in the menu.", 1, ["number"])
+let player = new objects.Actor("player", {name: "player"}, [], "main_menu", [], [], 0, 0, 0, false, 0)
+
 let help = new objects.Action("help", "", "Display a list of valid commands.", 0, [])
+let select = new objects.Action("select", "<option>", "Selects an option in the menu.", 1, ["number"])
+let stop = new objects.Action("stop", "", "End the Gojourn process.", 0, [])
 
 let list = {
     help: delete help,
-    select: delete select
+    select: delete select,
+    stop: delete stop
 }
 
 module.exports = {
     //Objects
+    cell01,
+    credits,
     main_menu,
     player,
     
     //Actions
-    select,
     help,
+    select,
+    stop,
 
     list
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dialog = require('./gojourn/dialog.json')
 let cur_loc = "main_menu"
 let a
 let loop = true
+let skip_cmd = false
 
 function rl(ask = "") { //Shorthand
     return (ask === "")
@@ -25,6 +26,7 @@ while (loop) {
     //Print dialogue
     switch(gj[cur_loc].me.name) {
         case "main_menu":
+            skip_cmd = false
             put(`
             ${dialog.main_menu.title01}\n
             ${dialog.main_menu.subtext01}\n
@@ -35,66 +37,100 @@ while (loop) {
             ${dialog.main_menu.option03}\n
             `)
             break
-    }
-    a = rl()
-    
-    //No input
-    if (a === "") {
-        put(dialog.error.no_command)
-        ok()
-        continue
-    }
-
-    //Invalid command
-    try {
-        if (gj[a.split(' ')[0]].me.name === "WW91IGZvdW5kIG1lISBIb3cgbWFnaWNhbC4=") put("Incredible.")
-    } catch {
-        put(dialog.error.invalid_command)
-        ok()
-        continue
-    }
- 
-    //Too many arguments
-    if (a.split(' ').splice(1, a.split(' ').length).length > gj[a.split(' ')[0]].me.arg_limit) {
-        put(dialog.error.too_many_args)
-        ok()
-        continue
-    }
-
-    //Argument types don't match up
-    if (
-        gj[a.split(' ')[0]].me.arg_limit >= 1 &&
-        a.split(' ').splice(1, a.split(' ').length).length === gj[a.split(' ')[0]].me.arg_limit
-    ) {
-        for (i = 0; i < gj[a.split(' ')[0]].me.arg_limit; i++) {
-            if (
-                (
-                    gj[a.split(' ')[0]].me.arg_types[i] === "string" &&
-                    !isNaN(a.split(' ').splice(1, a.split(' ').length)[i])
-                ) ||
-                (
-                    gj[a.split(' ')[0]].me.arg_types[i] === "number" &&
-                    isNaN(a.split(' ').splice(1, a.split(' ').length)[i])
-                )
-            ) {
-                put(dialog.error.incorrect_arg_type)
-                ok()
-                break
-            }
-        }
-    }
-
-    //Run command check
-    switch (a.split(' ')[0]) {
-        case "help":
-            put(`${dialog.misc.cmd_help_title01}\n`)
-            for (i = 0; i < Object.keys(gj.list).length; i++) {
-                put(`
-                    ${gj[Object.keys(gj.list)[i]].me.name} ${gj[Object.keys(gj.list)[i]].me.valid_args}\n
-                    \t${gj[Object.keys(gj.list)[i]].me.desc}
-                `)
-            }
+        case "credits":
+            skip_cmd = true
+            cur_loc = gj[cur_loc].me.adjacent.north
+            put(`
+                ${dialog.credits.label01}\n
+                ${dialog.credits.label02}\n\n
+                ${dialog.credits.label06}\n
+                ${dialog.credits.label04}\n
+                ${dialog.credits.label05}\n
+                ${dialog.credits.label03}\n\n
+                ${dialog.credits.label07}\n
+            `)
             ok()
             break
+    }
+
+    if (!skip_cmd) {
+        a = rl()
+    
+        //No input
+        if (a === "" && !skip_cmd) {
+            put(dialog.error.no_command)
+            ok()
+            continue
+        }
+
+        //Invalid command
+        try {
+            if (gj[a.split(' ')[0]].me.name === "WW91IGZvdW5kIG1lISBIb3cgbWFnaWNhbC4=") put("Incredible.")
+        } catch {
+            put(dialog.error.invalid_command)
+            ok()
+            continue
+        }
+ 
+        //Too many arguments
+        if (a.split(' ').splice(1, a.split(' ').length).length > gj[a.split(' ')[0]].me.arg_limit) {
+            put(dialog.error.too_many_args)
+            ok()
+            continue
+        }
+
+        //Argument types don't match up
+        if (
+            gj[a.split(' ')[0]].me.arg_limit >= 1 &&
+            a.split(' ').splice(1, a.split(' ').length).length === gj[a.split(' ')[0]].me.arg_limit
+        ) {
+            for (i = 0; i < gj[a.split(' ')[0]].me.arg_limit; i++) {
+                if (
+                    (
+                        gj[a.split(' ')[0]].me.arg_types[i] === "string" &&
+                        !isNaN(a.split(' ').splice(1, a.split(' ').length)[i])
+                    ) ||
+                    (
+                        gj[a.split(' ')[0]].me.arg_types[i] === "number" &&
+                        isNaN(a.split(' ').splice(1, a.split(' ').length)[i])
+                    )
+                ) {
+                    put(dialog.error.incorrect_arg_type)
+                    ok()
+                    break
+                }
+            }
+        }
+
+        //Run command check
+        switch (a.split(' ')[0]) {
+            case "help":
+                put(`${dialog.cmd.help.title01}\n`)
+                for (i = 0; i < Object.keys(gj.list).length; i++) {
+                    put(`
+                        ${gj[Object.keys(gj.list)[i]].me.name} ${gj[Object.keys(gj.list)[i]].me.valid_args}\n
+                        \t${gj[Object.keys(gj.list)[i]].me.desc}
+                    `)
+                }
+                ok()
+                break
+            case "select":
+                if (gj[cur_loc].me.name === "main_menu") {
+                    if (parseInt(a.split(' ').splice(1, a.split(' ').length)[0]) === 1) cur_loc = gj[cur_loc].me.adjacent.north
+                    else if (parseInt(a.split(' ').splice(1, a.split(' ').length)[0]) === 2) {
+                        put(dialog.cmd.select.saves_not_implemented)
+                        ok()
+                    }
+                    else if (parseInt(a.split(' ').splice(1, a.split(' ').length)[0]) === 3) cur_loc = gj[cur_loc].me.adjacent.south
+                    else {
+                        put(dialog.cmd.select.invalid_arg_choice)
+                        ok()
+                    }
+                }
+                break
+            case "stop":
+                cur_loc = gj.kill_location000.me.name //Will hardkill by throwing an error
+                break
+        }
     }
 }
