9d0acbb6269e4a773675da4c6c56d10014f74eb2
Blair Myhre <120442257+cocos-beans@users.noreply.github.com>
2023-12-14 11:30:53 -0600

    Delete gIC/build directory

--- gIC/build/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = class {
-  constructor() {
-    this.character = require('./objects/character.js')
-    this.item = require('./objects/item.js')
-    this.location = require('./objects/location.js')
-    this.player = require('./objects/player.js')
-    this.action = require('./action.js')
-  }
-}
